22dcc73e3591d8114fe9fd61273ea2790cc4b21e
victor soares <victor.soares@sintese.tech>
2024-11-24 13:38:22 -0300

    2024-11-24 - Tela login

---
+++ login/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+});
+
+async function onClickLogin() {
+    console.log('login');
+    try {
+        
+        //implementar tentativa de login
+        let successfullyLogin = true;  //se logar com sucesso, mudar para true;
+
+        if(successfullyLogin) {
+            onLoginSuccessfully()
+        } else {
+            onBadCredentials(`Email e/ou senha incorreto`)
+        }
+        
+    } catch (error) {
+        console.error(error);
+        return onBadCredentials(`Verificar conexão`);
+    }
+
+    
+}
+
+function onBadCredentials(msg) {
+    $('#alert_text_login').html(msg);
+}
+
+function onLoginSuccessfully() {
+    location.href = '/novo-curso'
+}
